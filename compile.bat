43a55a59b6b1d42dbb77b906f13f050f29d9beaa
decyg12 <michael1.reim@gmail.com>
2021-02-16 15:43:52 +0100

    initial commit 2

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 mkdir obj
-g++ -Wall -O3 -std=c++17 -I./lib/mingw-std-threads-master -IE:\Privat\Programmieren\Klassen\high_resolution_timer -c main.cpp -o obj/main.o
+g++ -Wall -O3 -std=c++17 -I./lib/mingw-std-threads-master -I./lib/high_resolution_timer -c main.cpp -o obj/main.o
 g++ -Wall -O3 -std=c++17 -I./lib/mingw-std-threads-master -c event_src/event_handler_src.cpp -o obj/event_handler_src.o
 g++ -Wall -O3 -std=c++17 -I./lib/mingw-std-threads-master -c event_src/listener_src.cpp -o obj/listener_src.o
 
